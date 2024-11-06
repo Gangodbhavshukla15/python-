30b85f74f3b7b0e13c89856669a015115d190b57
Gangodbhavshukla15 <142332102+Gangodbhavshukla15@users.noreply.github.com>
2024-11-06 16:59:42 +0530

    Add files via upload
     first commits to be done

---
+++ varable & Strings.py
@@ -0,0 +1,26 @@
+radius= float (input('please enter the circle radius:'))
+pi = 3.14159
+area = pi * radius**2
+print('You entered',radius,'the area of  the circle is ',area)
+
+
+far = float(input('please enter the temperature in fahrenheit:'))
+cel = (far - 32) * 5/9
+print(far,'fahrenheit in celsius is', cel)
+#  
+
+#  To obtain the product of two integers
+
+num_1 = int (input('please enter the first number:'))
+num_2 = int (input('please enter the secound number:'))
+product = num_1 * num_2
+print ('The product of', num_1, ' and ' , num_2, 'is',product)
+
+
+the sum of the two integers
+
+num_1 = int (input('please enter the first number:'))
+num_2 = int (input('please enter the second number:'))
+print('The sum of ' + str(num_1) +' and ' + str(num_2) + ' is ' + str(num_1 + num_2))
+
+
